63a21e756d6b2703ff01ed85ff772a5f8da4391a
tmoney82 <43918922+tmoney82@users.noreply.github.com>
2022-04-06 19:53:36 -0500

    first commit

--- src/Driver.java
+++ src/Driver.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Driver {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        //test
         Random r=new Random();
         /*
         m = number of rows in the first matrix
